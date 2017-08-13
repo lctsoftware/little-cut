4e6f3369e51e8619867109d6e9ae68345c2612a6
lctsoftware <1825362955@qq.com>
2017-08-13 08:25:35 +0800

    Add files via upload

---
+++ source/Little-cuts-Alpha-011.py
@@ -0,0 +1,31 @@
+import time
+import os
+word = ['泥菩萨过河自身难保', '小葱拌豆腐一清二白', '隔着门缝看人把人看扁了']
+while True:
+ try:
+  a=input('管理小斩斩，请输入密码（输入密码进入后再输exit退出）')
+  if a=='admin':
+   while True:
+    try:
+      s=input('请输入命令')
+      if s=='exit':
+         break
+    except:
+     print('输入错误，等待1秒')
+     time.sleep(1)
+  elif a=='break':
+     break
+  wait=int(input('请输入你背每条歇后语的思考时间（秒数）'))
+  print('快去背诵歇后语吧:')
+  for key in word:
+    print(key)
+    time.sleep(wait)
+  os.system('cls')
+  lis=','.join(word)
+  inp=input('请输入你刚才背的全部歇后语(两句之间用英文逗号分隔，按顺序来哦！)')
+  if lis==inp:
+    print("Bingo!")
+  else:
+    print("Ahoh!")
+ except:
+    print('输入错误，等待1秒')
